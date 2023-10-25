293bf6c70de9c8ef0c03b43819718fb15793e8c7
Dide van Berkel <dide.vanberkel@gmail.com>
2023-10-25 14:43:41 +0200

    Initial Commit

---
+++ Places/PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesApp.swift
+//  Places
+//
+//  Created by Dide van Berkel on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
